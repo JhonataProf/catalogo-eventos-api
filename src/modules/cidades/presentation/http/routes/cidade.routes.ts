caedfdb1cd4676602acf2f638e9e194ffc4265ff
Professor Jhonata <jhonata.sobral@ms.senac.br>
2026-02-19 14:23:43 -0400

    inclui injeção de dependências

--- src/modules/cidades/presentation/http/routes/cidade.routes.ts
+++ src/modules/cidades/presentation/http/routes/cidade.routes.ts
@@ -7,9 +7,13 @@ import {
   UpdateCidadeController,
 } from "../controller";
 import { FindCidadeByIdController } from "../controller/find-cidade-by-id.controller";
+import { CreateCidadeUseCase } from "@/modules/cidades/application/use-cases";
+import { SequelizeCidadeRepository } from "@/modules/cidades/infra/sequelize/sequelize-city.repository";
 
 export function registerCidadeRoutes(router: Router): void {
-  router.post("/cidades", adaptRoute(new CreateCidadeController()));
+  const cidadeRepo = new SequelizeCidadeRepository();
+  const createCidadeUseCase = new CreateCidadeUseCase(cidadeRepo, cidadeRepo)
+  router.post("/cidades", adaptRoute(new CreateCidadeController(createCidadeUseCase)));
   router.get("/cidades", adaptRoute(new ListCidadeController()));
   router.put("/cidades/:id", adaptRoute(new UpdateCidadeController()));
   router.delete("/cidades/:id", adaptRoute(new DeleteCidadeController()));
